88b431b82b1eb6531d6fc92e01ddc19fc26aeb78
ola-alison <ola.alison@gmail.com>
2018-03-05 22:26:02 +0100

    added draw function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,38 @@
-const words = ['Hello', 'world'];
-const styleTypes = ['scss', 'less'];
-const h1 = document.createElement('h1');
-h1.innerHTML = styleTypes
-    .map((type, index) => `<span class="${type}">${words[index]}</span>`)
-    .join(' ');
-document.body.appendChild(h1);
-console.log(h1);
+
+let phraseCollection = [];
+let input = document.getElementById("phrase-item");
+let btnAdd = document.getElementById("button-add");
+let btnDraw = document.getElementById("button-draw");
+
+btnAdd.onclick = function addPhrase() {
+
+  let phrase = input.value;
+  let phraseP = document.createElement("p");
+
+  if (phrase !== "") {
+    phraseCollection.push(phrase);
+  }
+  else {
+    alert("insert phrase");
+  }
+
+  console.log(phraseCollection);
+
+  document.getElementById("phrase-list").appendChild(phraseP).innerHTML = phrase;
+}
+
+btnDraw.onclick = function drawPhrase() {
+
+  let phraseRandom;
+
+  if (Object.keys(phraseCollection).length > 1) {
+    phraseRandom = phraseCollection[Math.floor(Math.random()*phraseCollection.length)];
+  }
+  else {
+    phraseRandom = "*** add something first ***";
+  }
+
+  console.log(phraseRandom);
+
+  document.getElementById("phrase-random").innerHTML = phraseRandom;
+}

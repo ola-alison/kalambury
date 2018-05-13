11744818692dd77ddd2b46b5c79c58b5af95e3e1
ola-alison <ola.alison@gmail.com>
2018-05-13 18:53:48 +0200

    first working version with ajax, no styles

--- src/app.js
+++ src/app.js
@@ -1,39 +1,210 @@
+import $ from 'jquery';
 
-let phraseCollection = ["pies", "kot", "chomik", "żółw", "papuga"];
+const API = "http://todo.fatco.de/todo/";
+
+let phraseCollection = [];
+
 let input = document.getElementById("phrase-item");
 let btnAdd = document.getElementById("button-add");
 let btnDraw = document.getElementById("button-draw");
+let phrase = {};
 
-btnAdd.onclick = function addPhrase() {
+// --------------------- ADD NEW PHRASE -----------------------
+let addPhrase = () => {
 
-  let phrase = input.value;
-  let phraseP = document.createElement("p");
+  if (input.value !== "") {
+    phrase = {
+      item: input.value,
+      status: "active"
+    };
 
-  if (phrase !== "") {
+    document.getElementById("new-phrase").innerHTML = phrase.item;
+    document.getElementById("phrase-added").classList.add("visible");
+    document.getElementById("phrase-added").classList.remove("hidden");
     phraseCollection.push(phrase);
+
+    $.ajax( {
+      url: API,
+      method: "POST",
+      data: phrase,
+      success: function(resp) {
+        console.log(phrase, resp);
+      },
+      error: function(resp) {
+        console.log(resp);
+      }
+    })
+
+    input.value = "";
+    showPhrases();
   }
   else {
-    alert("insert phrase");
+    alert("Insert phrase first.");
   }
 
-  console.log(phraseCollection);
-
-  document.getElementById("phrase-list").appendChild(phraseP).innerHTML = phrase;
-  input.value = "";
 }
+
+btnAdd.onclick = addPhrase.bind();
+
+
+// --------------------------------------------
+
+let clearList = () => {
+  document.getElementById("all-phrases-list").innerHTML = "";
+}
+
+
+let removePhrase = function() {
+
+  this.parentNode.remove();
+
+  let toBeRemoved = this.parentNode.getAttribute("data-id");
+  console.log(API.length);
+
+  $.ajax( {
+    url: API + toBeRemoved,
+    method: "DELETE",
+    success: function(resp) {
+    },
+    error: function(resp) {
+    }
+  })
+
+}
+
+
+let deactivatePhrase = function() {
+
+  let toBeDeactivated = this.parentNode.getAttribute("data-id");
+
+  this.parentNode.classList.add("inactive");
+  this.parentNode.classList.remove("active");
+
+
+  $.ajax( {
+    url: API + toBeDeactivated,
+    method: "PATCH",
+    data: {status: 'inactive'},
+    success: function(resp) {
+      showPhrases();
+    },
+    error: function(resp) {
+    }
+  })
+}
+
+
+let activatePhrase = function() {
+
+  let toBeActivated = this.parentNode.getAttribute("data-id");
+
+  this.parentNode.classList.add("active");
+  this.parentNode.classList.remove("inactive");
+
+  $.ajax( {
+    url: API + toBeActivated,
+    method: "PATCH",
+    data: {status: 'active'},
+    success: function(resp) {
+      showPhrases();
+    },
+    error: function(resp) {
+    }
+  })
+}
+
+
+// --------------------- DRAW PHRASE -----------------------
 
 btnDraw.onclick = function drawPhrase() {
 
-  let phraseRandom1, phraseRandom2;
+  let phraseRandom;
 
-  phraseRandom1 = phraseCollection[Math.floor(Math.random()*phraseCollection.length)];
-  phraseRandom2 = phraseCollection[Math.floor(Math.random()*phraseCollection.length)];
+  let activePhrases = phraseCollection.filter(function(phrase) {
+    return phrase.status === "active";
+  });
 
-  while (phraseRandom2 === phraseRandom1) {
-    phraseRandom2 = phraseCollection[Math.floor(Math.random()*phraseCollection.length)];
+
+  if (activePhrases.length > 0) {
+    phraseRandom = activePhrases[Math.floor(Math.random()*activePhrases.length)];
+
+    $.ajax( {
+      url: API + phraseRandom.id,
+      method: "PATCH",
+      data: {status: 'inactive'},
+      success: function(resp) {
+        showPhrases();
+      },
+      error: function(resp) {
+      }
+    })
+
+    document.getElementById("phrase-random").innerHTML = `Your phrase is: <strong>${phraseRandom.item}</strong>`;
   }
 
-  console.log(phraseRandom1, phraseRandom2);
+  else {
+    alert("There are no active phrases to draw from.");
+  }
 
-  document.getElementById("phrase-random").innerHTML = `wybierz hasło: <strong>${phraseRandom1}</strong> lub <strong>${phraseRandom2}</strong>`;
 }
+
+
+// --------------------- PRINT PHRASES -----------------------
+let showPhrases = () => {
+
+  clearList();
+
+  $.ajax( {
+    url: API,
+    method: "GET",
+    success: function(resp) {
+      phraseCollection.length = 0;
+      for (let i = 0; i < resp.length; i++) {
+
+        let phrase = resp[i];
+
+        phraseCollection.push(phrase);
+
+        let allPhrasesList = document.getElementById("all-phrases-list");
+        let phraseCollectionP = document.createElement("p");
+
+        let phraseCollectionDeactivate = document.createElement("button");
+        let phraseCollectionActivate = document.createElement("button");
+        let phraseCollectionRemove = document.createElement("button");
+
+        phraseCollectionP.setAttribute('data-id', phraseCollection[i].id);
+        phraseCollectionP.className = phraseCollection[i].status;
+
+        console.log(phraseCollection[i]);
+
+        allPhrasesList.appendChild(phraseCollectionP).innerHTML = '<span class="all-phrases-list-item">' + phraseCollection[i].item + '</span>';
+
+        allPhrasesList.appendChild(phraseCollectionP).appendChild(phraseCollectionDeactivate).innerHTML = 'deactivate';
+        phraseCollectionDeactivate.classList.add("btn-deactivate", "button");
+
+        allPhrasesList.appendChild(phraseCollectionP).appendChild(phraseCollectionActivate).innerHTML = 'activate';
+        phraseCollectionActivate.classList.add("btn-activate", "button");
+
+        allPhrasesList.appendChild(phraseCollectionP).appendChild(phraseCollectionRemove).innerHTML = "remove";
+        phraseCollectionRemove.classList.add("btn-remove", "button");
+
+        phraseCollectionRemove.onclick = removePhrase.bind(phraseCollectionRemove);
+        phraseCollectionDeactivate.onclick = deactivatePhrase.bind(phraseCollectionDeactivate);
+        phraseCollectionActivate.onclick = activatePhrase.bind(phraseCollectionActivate);
+      };
+
+      console.log("get ", phraseCollection);
+    },
+
+    error: function(resp) {
+      console.log("Error", resp);
+    },
+  })
+}
+
+showPhrases();
+
+
+$('.kalambury-manage-phrases-btn').on('click',function(e){
+  $(this).parent().toggleClass('open');
+});
